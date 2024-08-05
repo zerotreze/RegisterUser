fd4ae4f3ea667e1a1772428480936282c6ecfd07
José Vitor <blackdarkfur@hotmail.com>
2024-08-05 14:42:29 -0300

    first commit

---
+++ projeto_cad_usuarios/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Usuario
+
+def home(request):
+    return render(request,'usuarios/home.html')
+
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    return render(request,'usuarios/usuarios.html',usuarios)
